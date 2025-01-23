3b1d24b91646dbaa36447b07e2a46534fa066c38
fzahrae <fatimazahraeejjafri@gmail.com>
2025-01-23 09:53:32 +0100

    7c236b

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,16 +17,13 @@ $routes->post('login', 'Login::auth');
 $routes->get('dashbord', 'Login::dashboard'); // Displays the professor dashboard
 $routes->get('etudiant', 'Login::etudiant'); // Displays the student dashboard
 
-<<<<<<< HEAD
 $routes->get('filiere/getFilieresByProf/(:num)', 'FiliereController::getFilieresByProf/$1');
 $routes->get('module/getModulesByFiliereAndProf/(:num)/(:num)', 'ModuleController::getModulesByFiliereAndProf/$1/$2');
 $routes->get('etudiant/getStudentsByFiliere/(:num)', 'EtudiantController::getStudentsByFiliere/$1');
 $routes->get('etudiant/getStudentsByFiliereAndModule/(:num)/(:num)', 'EtudiantController::getStudentsByFiliereAndModule/$1/$2');
-=======
 $routes->get('logout', 'Login::logout');
 $routes->get('reclamation', 'Login::reclamation'); // Displays the student dashboard
 $routes->get('reclamation_prof', 'Login::reclamation_prof'); // Displays the student dashboard
->>>>>>> 7c236b7fda55bdae3d1a3b0af2c2e9a25443f416
 
 $routes->post('insertGrades', 'NoteController::insertGrades');
 $routes->post('importExcel', 'NoteController::insertGradesFromExcel');
